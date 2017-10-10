02b819e59015d0ace551181f4443a4ff0ec8509b
Luuk Wuijster <luuk.wuijster@hotmail.com>
2017-10-10 14:24:26 +0200

    Login

--- PEV/PEV/Models/AccountViewModels/LoginViewModel.cs
+++ PEV/PEV/Models/AccountViewModels/LoginViewModel.cs
@@ -16,7 +16,7 @@ namespace PEV.Models.AccountViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Ondhoud mij?")]
         public bool RememberMe { get; set; }
     }
 }
