c2ba125af696ea898fbdbb1e0fac53a8cc50e1ed
Meriyem Bilgili <97034339@st.deltion.nl>
2017-10-31 13:08:26 +0100

    registreer systeem

--- PEV/PEV/Models/AccountViewModels/RegisterViewModel.cs
+++ PEV/PEV/Models/AccountViewModels/RegisterViewModel.cs
@@ -9,9 +9,29 @@ namespace PEV.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required] 
+        [StringLength(20)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Must have a Date of Birth (MM/DD/YYYY)")]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public string DateOfBirth { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
@@ -23,5 +43,7 @@ namespace PEV.Models.AccountViewModels
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        
     }
 }
